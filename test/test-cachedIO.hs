2c54b467a265ba68f4d1a017953a63da9df2368a
Ethan Glasser-Camp <ethan@betacantrips.com>
2015-03-16 15:17:11 -0400

    test-cachedIO: add
    This is better than an example because it compiles.

---
+++ test/test-cachedIO.hs
@@ -0,0 +1,21 @@
+import Control.Concurrent.CachedIO (cachedIO)
+import Data.List (isInfixOf)
+
+crawlTheInternet :: IO [String]
+crawlTheInternet = do
+    putStrLn "Scanning pages.."
+    putStrLn "Parsing HTML.."
+    putStrLn "Following links.."
+    return ["website about Haskell", "website about Ruby", "slashdot.org",
+            "The Monad.Reader", "haskellwiki"]
+
+searchEngine :: String -> IO [String] -> IO [String]
+searchEngine query internet = do
+    pages <- internet
+    return $ filter (query `isInfixOf`) pages
+
+main :: IO ()
+main = do
+    cachedInternet <- cachedIO 600 crawlTheInternet   -- 10 minute cache
+    print =<< searchEngine "Haskell" cachedInternet
+    print =<< searchEngine "C#" cachedInternet
