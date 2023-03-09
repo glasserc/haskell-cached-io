91977a8a47e895eb12d3f14c25196022010dd953
lrworth <luke.worth@bellroy.com>
2023-03-10 10:04:53 +1100

    Add haskell-language-server (#10)
    * add HLS to dev shell. create direnv environment
    
    * .gitignore .envrc
    
    * flake.nix: sort buildInputs
    
    * Add hie.yaml
    
    * flake.nix: refactor to name compilers once
    
    ---------
    
    Co-authored-by: josemarialanda <josemaria.landa@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,42 +2,50 @@
   description = "A simple library to cache a single IO action with timeout";
 
   inputs = {
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import inputs.nixpkgs { inherit system; };
-        makePackage = haskellPackages: haskellPackages.callPackage ./cached-io.nix { };
+        pkgs = import nixpkgs { inherit system; };
       in
       rec
       {
-        packages = {
-          default = makePackage pkgs.haskellPackages;
-          ghc884 = makePackage pkgs.haskell.packages.ghc884;
-          ghc8107 = makePackage pkgs.haskell.packages.ghc8107;
-          ghc902 = makePackage pkgs.haskell.packages.ghc902;
-          ghc921 = makePackage pkgs.haskell.packages.ghc921;
-        };
+        # This becomes the list of package and devShell names.
+        supportedCompilers = [
+          "ghc884"
+          "ghc8107"
+          "ghc902"
+          "ghc926"
+        ];
 
-        devShells = builtins.mapAttrs
-          (_: v: v.env.overrideAttrs (oldAttrs: {
+        packages = builtins.listToAttrs (
+          builtins.map
+            (compiler: {
+              name = compiler;
+              value = pkgs.haskell.packages.${compiler}.callPackage ./cached-io.nix { };
+            })
+            supportedCompilers
+        );
+
+        devShells' = (builtins.mapAttrs
+          (compiler: v: v.env.overrideAttrs (oldAttrs: {
             buildInputs = oldAttrs.buildInputs
               ++ [ pkgs.nixpkgs-fmt ]
-              ++ (with pkgs.haskellPackages; [
+              ++ (with pkgs.haskell.packages.${compiler}; [
               cabal-fmt
               cabal-install
               doctest
               haskell-ci
+              haskell-language-server
               hlint
             ]);
           }))
-          packages;
+          packages
+        );
+
+        devShells = devShells' // { default = devShells'.ghc926; };
       });
 }
